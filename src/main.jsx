f7e53dca26f1f8fc748750120540f42f1b70f96a
Siser Pratap <siserinsevoc@gmail.com>
2024-08-12 16:03:09 +0530

    Initialized main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
+import {HomePage, ChatPage, DashBoard, SignInPage, SignUpPage} from "./routes"
+import RootLayout from './layouts/RootLayout.jsx'
+import DashboardLayout from './layouts/DashboardLayout.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element:<HomePage />
+      },
+      {
+        path:"/sign-in/*",
+        element:<SignInPage />
+      },
+      {
+        path:"/sign-up/*",
+        element:<SignUpPage />
+      },
+      {
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <DashBoard />,
+          },
+          {
+            path: "/dashboard/chats/:id",
+            element: <ChatPage />,
+          },
+        ],
+      },
+    ]
+    }]);
+
+
+    ReactDOM.createRoot(document.getElementById("root")).render(
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
+    );
