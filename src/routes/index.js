803feb74bdc3c3b3da28d4cca54cbcd513be627f
Siser Pratap <siserinsevoc@gmail.com>
2024-08-12 16:08:27 +0530

    Initialized clerk API

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import HomePage from "./HomePage";
+import ChatPage from "./ChatPage";
+import DashBoard from "./DashBoard";
+import SignInPage from "./SignInPage";
+import SignUpPage from "./SignUpPage";
+
+
+
+
+export {
+    HomePage,
+    ChatPage,
+    DashBoard,
+    SignInPage,
+    SignUpPage
+}
