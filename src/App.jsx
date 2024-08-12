00f362e5e102ee76dc92c16294b0e06dbfa90a89
Siser Pratap <siserinsevoc@gmail.com>
2024-08-12 16:04:08 +0530

    Added App.jsx App.css index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-        
-    </>
+    <div>Hello World</div>
   )
 }
 
